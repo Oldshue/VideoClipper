1bcd091df6453312e909e9d052de146426246468
Oldshue <130860400+Oldshue@users.noreply.github.com>
2025-01-25 08:31:17 -0600

    Delete src directory

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import YoutubeClipper from '../components/VideoClipper';
-
-export default function Home() {
-  return (
-    <main className="container mx-auto p-4">
-      <YoutubeClipper />
-    </main>
-  );
-}
